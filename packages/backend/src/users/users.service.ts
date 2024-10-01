cb6e7e7ab9b1b3028710e7e143d2f91d4a5cbbb8
Omar Bahutair <o.bahutair@gmail.com>
2024-10-01 21:21:32 +0400

    feat(backend): create read and update methods for users service

--- packages/backend/src/users/users.service.ts
+++ packages/backend/src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { User, UserDocument } from './user.schema';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public constructor(
+    @InjectModel(User.name) private readonly userModel: Model<User>,
+  ) {}
+
+  public create(user: User): Promise<UserDocument> {
+    return this.userModel.create(user);
+  }
+
+  public findById(id: string): Promise<UserDocument | null> {
+    if (!isValidObjectId(id)) return null;
+
+    return this.userModel.findById(id);
+  }
+
+  public update(id: string, user: User): Promise<UserDocument | null> {
+    if (!isValidObjectId(id)) return null;
+
+    return this.userModel.findByIdAndUpdate(id, user);
+  }
+}
