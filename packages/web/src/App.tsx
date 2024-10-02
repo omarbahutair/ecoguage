158081e263b036cf897c6533d82f9ff2516dfef6
Omar Bahutair <o.bahutair@gmail.com>
2024-10-02 11:39:19 +0400

    feat(web): setup routing

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,17 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
-  return <div>Hello World</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
