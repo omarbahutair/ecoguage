3fedb0008f8173a29c0af8d210cd8b9649f98812
Omar Bahutair <o.bahutair@gmail.com>
2024-10-23 11:20:13 +0400

    feat(web): refreshed auth state at window reload

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
+import { store } from './store';
+import { refreshAuth } from './store/auth-slice';
 
 export default function App() {
+  useEffect(() => {
+    store.dispatch(refreshAuth());
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
