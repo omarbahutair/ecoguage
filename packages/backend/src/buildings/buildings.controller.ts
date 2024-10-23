720272d1fb713bfd0d03507bc2eb4bfaca71b70b
Omar Bahutair <o.bahutair@gmail.com>
2024-10-23 11:44:06 +0400

    feat(backend): update building name

--- packages/backend/src/buildings/buildings.controller.ts
+++ packages/backend/src/buildings/buildings.controller.ts
@@ -1,6 +1,6 @@
-import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { Body, Controller, Param, Post, Put, UseGuards } from '@nestjs/common';
 import { BuildingsService } from './buildings.service';
-import { CreateBuildingDto } from './dtos/create-building.dto';
+import { UpsertBuildingDto } from './dtos/upsert-building.dto';
 import { AuthGuard } from 'src/auth/guards/auth.guard';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { UserDocument } from 'src/users/user.schema';
@@ -12,9 +12,19 @@ export class BuildingsController {
   @Post()
   @UseGuards(AuthGuard)
   public create(
-    @Body() createBuilding: CreateBuildingDto,
+    @Body() createBuilding: UpsertBuildingDto,
     @CurrentUser() user: UserDocument,
   ) {
     return this.buildingsService.create(createBuilding, user);
+  }
+
+  @Put(':id')
+  @UseGuards(AuthGuard)
+  public udpate(
+    @Param('id') id: string,
+    @Body() createBuilding: UpsertBuildingDto,
+    @CurrentUser() user: UserDocument,
+  ) {
+    return this.buildingsService.update(id, createBuilding, user);
   }
 }
--- packages/backend/src/buildings/buildings.service.ts
+++ packages/backend/src/buildings/buildings.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Building } from './building.schema';
-import { Model } from 'mongoose';
-import { CreateBuildingDto } from './dtos/create-building.dto';
+import { isValidObjectId, Model } from 'mongoose';
+import { UpsertBuildingDto } from './dtos/upsert-building.dto';
 import { UserDocument } from 'src/users/user.schema';
 
 @Injectable()
@@ -11,10 +15,32 @@ export class BuildingsService {
     @InjectModel(Building.name) private readonly buildingModel: Model<Building>,
   ) {}
 
-  public async create(createBuilding: CreateBuildingDto, user: UserDocument) {
+  public async create(createBuilding: UpsertBuildingDto, user: UserDocument) {
     return this.buildingModel.create({
       ...createBuilding,
       userId: user._id,
     });
+  }
+
+  public async update(
+    id: string,
+    updateBuilding: UpsertBuildingDto,
+    user: UserDocument,
+  ) {
+    if (!isValidObjectId(id))
+      throw new BadRequestException('Invalid building ID');
+
+    const updatedBuilding = await this.buildingModel.findOneAndUpdate(
+      {
+        _id: id,
+        userId: user._id,
+      },
+      updateBuilding,
+      { new: true },
+    );
+
+    if (!updateBuilding) throw new NotFoundException('Building not found');
+
+    return updatedBuilding;
   }
 }
