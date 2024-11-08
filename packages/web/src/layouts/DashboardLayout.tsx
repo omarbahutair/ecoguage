d897a7715f61440770840e4ca84ef0db81f93b5e
Omar Bahutair <o.bahutair@gmail.com>
2024-11-08 21:13:56 +0400

    feat(web): logout button

--- packages/web/src/layouts/DashboardLayout.tsx
+++ packages/web/src/layouts/DashboardLayout.tsx
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
-import { Outlet } from 'react-router';
+import { Outlet, useNavigate } from 'react-router';
 import SideBar from '../components/SideBar';
 import Logo from '../components/Logo';
 import ExtenderButton from '../components/ExtenderButton';
+import { logout } from '../store/auth-slice';
+import { dispatch } from '../store';
 
 export default function DashboardLayout() {
   const [isExtended, setIsExtended] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <div className="h-[100dvh] w-screen overflow-hidden flex flex-col">
@@ -34,7 +37,7 @@ export default function DashboardLayout() {
           className={`absolute lg:relative w-full lg:max-w-80 h-full p-2 overflow-auto transition-all duration-500 ${isExtended ? 'backdrop-blur-sm bg-white bg-opacity-50 z-50' : 'lg:-translate-x-full lg:!max-w-0 lg:overflow-hidden opacity-0 -z-50'}`}
         >
           <div
-            className={`w-full lg:h-full transition-all duration-500 ${isExtended ? '' : '-translate-x-[110%] lg:-translate-x-0'}`}
+            className={`flex flex-col justify-between sm:gap-1 w-full h-full transition-all duration-500 ${isExtended ? '' : '-translate-x-[110%] lg:-translate-x-0'}`}
           >
             <SideBar
               onLinkClick={() => {
@@ -42,8 +45,18 @@ export default function DashboardLayout() {
                   setIsExtended(false);
                 }
               }}
-              className={`bg-primary-fade w-full h-full max-w-xl m-auto rounded-lg`}
+              className={`bg-primary-fade w-full lg:h-full h-fit max-w-xl mx-auto rounded-lg`}
             />
+            <button
+              onClick={() => {
+                dispatch(logout());
+                navigate('login');
+              }}
+              className="bg-white border border-red-600 text-red-600 p-3 rounded-lg flex items-center justify-center gap-3"
+            >
+              <i className="fa-solid fa-arrow-right-from-bracket" />
+              Logout
+            </button>
           </div>
         </div>
         <Outlet />
--- packages/web/src/store/index.ts
+++ packages/web/src/store/index.ts
@@ -10,3 +10,5 @@ export const store = configureStore({
     auth: authReducer,
   },
 });
+
+export const dispatch = store.dispatch;
