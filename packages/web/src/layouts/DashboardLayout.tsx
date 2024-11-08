bf0fa233e51108ad506a4d2af9df81a7441e857c
Omar Bahutair <o.bahutair@gmail.com>
2024-11-08 21:18:43 +0400

    feat(web): protected the dashboard layout from unauthenticated users

--- packages/web/src/layouts/DashboardLayout.tsx
+++ packages/web/src/layouts/DashboardLayout.tsx
@@ -4,11 +4,18 @@ import SideBar from '../components/SideBar';
 import Logo from '../components/Logo';
 import ExtenderButton from '../components/ExtenderButton';
 import { logout } from '../store/auth-slice';
-import { dispatch } from '../store';
+import { dispatch, IStore } from '../store';
+import { useSelector } from 'react-redux';
 
 export default function DashboardLayout() {
   const [isExtended, setIsExtended] = useState(false);
   const navigate = useNavigate();
+  const auth = useSelector((store: IStore) => store.auth);
+
+  if (!auth.value) {
+    navigate('/login');
+    return null;
+  }
 
   return (
     <div className="h-[100dvh] w-screen overflow-hidden flex flex-col">
@@ -37,7 +44,7 @@ export default function DashboardLayout() {
           className={`absolute lg:relative w-full lg:max-w-80 h-full p-2 overflow-auto transition-all duration-500 ${isExtended ? 'backdrop-blur-sm bg-white bg-opacity-50 z-50' : 'lg:-translate-x-full lg:!max-w-0 lg:overflow-hidden opacity-0 -z-50'}`}
         >
           <div
-            className={`flex flex-col justify-between sm:gap-1 w-full h-full transition-all duration-500 ${isExtended ? '' : '-translate-x-[110%] lg:-translate-x-0'}`}
+            className={`flex flex-col max-w-xl mx-auto justify-between sm:gap-1 w-full h-full transition-all duration-500 ${isExtended ? '' : '-translate-x-[110%] lg:-translate-x-0'}`}
           >
             <SideBar
               onLinkClick={() => {
@@ -45,12 +52,12 @@ export default function DashboardLayout() {
                   setIsExtended(false);
                 }
               }}
-              className={`bg-primary-fade w-full lg:h-full h-fit max-w-xl mx-auto rounded-lg`}
+              className={`bg-primary-fade w-full lg:h-full h-fit rounded-lg`}
             />
             <button
               onClick={() => {
                 dispatch(logout());
-                navigate('login');
+                navigate('/login');
               }}
               className="bg-white border border-red-600 text-red-600 p-3 rounded-lg flex items-center justify-center gap-3"
             >
